0d2b7b97cbc89e497dd5b3c844484f0a787da4eb
AmitaiB <blickstein@gmail.com>
2015-08-27 00:15:58 -0400

    fetches dances in prepare for segue

--- searchAndShout/FISDancesTableViewController.m
+++ searchAndShout/FISDancesTableViewController.m
@@ -113,15 +113,17 @@
 }
 */
 
-/*
+
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
 {
+    FISDataStore *datastore = [FISDataStore sharedDataStore];
+    [datastore fetchDances];
     // Get the new view controller using [segue destinationViewController].
     // Pass the selected object to the new view controller.
 }
-*/
+
 
 @end
